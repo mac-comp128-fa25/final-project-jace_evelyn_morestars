72d625dada397af9eb5c4a7bd47e707b12b07bba
jace-co <pen.ink.me@gmail.com>
2025-11-24 14:57:51 -0600

    flushing out starPhase and starTree

--- src/StarTree.java
+++ src/StarTree.java
@@ -4,15 +4,24 @@ import java.util.ArrayList;
 public class StarTree<StarPhase> {
     
     private List<StarTree<StarPhase>> children = new ArrayList<>();
-    private StarTree<StarPhase> parent; // ?
-    private StarPhase phaseInfo; // keep info about star in here
+    private StarPhase parent;
+    private StarPhase phaseInfo;
 
 
     public StarTree(StarPhase phaseInfo){
         this.phaseInfo = phaseInfo;
     }
 
-    public void addPhase(StarTree<StarPhase> newPhase){ // TODO: argument for location of new node
+    public void addPhase(StarTree<StarPhase> newPhase){
+        newPhase.setParent(this);
+        children.add(newPhase);
+    }
 
+    public void setParent(StarTree<StarPhase> parent){
+
+    }
+
+    public StarPhase getParent(){
+        return parent;
     }
 }
